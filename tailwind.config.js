3a4bc23bf5e4bcb3a183cff2ebcc37f61eebac81
SavindiLayanga <savindilayanga@gmail.com>
2024-12-08 14:37:52 +0530

    .

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all your component files
+    "./public/index.html",        // If you're using HTML files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
